1c623c7d53ca82381b965894a584053410f38a8e
beranemo <beranemo@gmail.com>
2018-05-08 11:32:11 +0800

    完成新增文章能成功寫入資料庫功能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,21 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+  
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "文章已成功新增"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "文章新增失敗"
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
