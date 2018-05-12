befe51ebb61bcb23b746f082a8175112499d2a4c
beranemo <beranemo@gmail.com>
2018-05-12 16:46:05 +0800

    時區改為台北時區

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module DojoForum
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    
+    # 設置完時區要重新啓動 rails s 才會生效
+    config.time_zone = "Taipei"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
