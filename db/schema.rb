53e366fdd8120106af8c78561dd04b48181ea984
beranemo <beranemo@gmail.com>
2018-05-12 15:44:12 +0800

    文章清單上加入回覆總數顯示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509001326) do
+ActiveRecord::Schema.define(version: 20180512073727) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180509001326) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "comments_count", default: 0
   end
 
   create_table "users", force: :cascade do |t|
