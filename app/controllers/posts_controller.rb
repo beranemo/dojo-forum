d6c654d447acec22e340305656447fc32308bea6
beranemo <beranemo@gmail.com>
2018-05-12 17:11:53 +0800

    文章回覆內容每則顯示20筆

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,7 @@ class PostsController < ApplicationController
   
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments.page(params[:page]).per(20)
     @comment = Comment.new
     impressionist(@post, "message...")
   end
