58661bac3ed8d96ccccb83c420c9e2015a596da1
beranemo <beranemo@gmail.com>
2018-05-12 22:11:28 +0800

    更新model註釋

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,16 @@
 #
 # Table name: posts
 #
-#  id         :integer          not null, primary key
-#  title      :string
-#  content    :text
-#  status     :string
-#  user_id    :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                :integer          not null, primary key
+#  title             :string
+#  content           :text
+#  status            :string
+#  user_id           :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  image             :string
+#  comments_count    :integer          default(0)
+#  last_comment_time :datetime
 #
 
 class Post < ApplicationRecord
