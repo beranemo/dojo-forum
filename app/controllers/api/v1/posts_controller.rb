8f0f59d70c17e40a8522e07a4ee2e2799a51215a
beranemo <beranemo@gmail.com>
2018-05-14 05:34:39 +0800

    設定API:posts#show

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::PostsController < ApiController
   def index
     @posts = Post.all
-    render json: @posts
+    render json: {
+      data: @posts
+    }
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+    render json: {
+      title: @post.title,
+      content: @post.content
+    }
   end
 end
