fd62a06e1ae526009b4d589222591632b5e06f15
beranemo <beranemo@gmail.com>
2018-05-14 06:43:23 +0800

    實作api登入／登出功能

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,10 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      
+      post "/login" => "auth#login"
+      post "/logout" => "auth#logout"
+      
       resources :posts, only: [:index, :show, :create, :update, :destroy]
     end
   end
