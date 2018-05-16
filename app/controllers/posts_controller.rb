62edc12b78d76d8bce03d5f1366101702af87e93
beranemo <beranemo@gmail.com>
2018-05-16 16:36:27 +0800

    未登入應該也能排序

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,6 @@ class PostsController < ApplicationController
     @categories = Category.all
     
     @order_tag = params[:tag]
-
     
     if user_signed_in?
       if @order_tag == "last_comment_time"
@@ -18,9 +17,16 @@ class PostsController < ApplicationController
       else
         @posts = Post.all.where(status: "craft").check_who_can_see(current_user).order(id: :desc).page(params[:page]).per(20)
       end
-      
     else
-      @posts = Post.all.where(status: "craft", who_can_see: "all").order(id: :desc).page(params[:page]).per(20)
+      if @order_tag == "last_comment_time"
+        @posts = Post.all.where(status: "craft", who_can_see: "all").order(last_comment_time: :desc).page(params[:page]).per(20)
+      elsif @order_tag == "comments_count"
+        @posts = Post.all.where(status: "craft", who_can_see: "all").order(comments_count: :desc).page(params[:page]).per(20)
+      elsif @order_tag == "impressionist_count"
+        @posts = Post.all.where(status: "craft", who_can_see: "all").order(impressionist_count: :desc).page(params[:page]).per(20)
+      else
+        @posts = Post.all.where(status: "craft", who_can_see: "all").order(id: :desc).page(params[:page]).per(20)
+      end
     end
   end
   
