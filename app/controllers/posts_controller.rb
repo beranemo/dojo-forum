02a97b0ba40804e933d913d657edb77713628e05
beranemo <beranemo@gmail.com>
2018-05-16 16:31:20 +0800

    在首面加入文章排序方式

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,21 @@ class PostsController < ApplicationController
   
   def index
     @categories = Category.all
+    
+    @order_tag = params[:tag]
+
+    
     if user_signed_in?
-      @posts = Post.all.where(status: "craft").check_who_can_see(current_user).order(id: :desc).page(params[:page]).per(20)
+      if @order_tag == "last_comment_time"
+        @posts = Post.all.where(status: "craft").check_who_can_see(current_user).order(last_comment_time: :desc).page(params[:page]).per(20)
+      elsif @order_tag == "comments_count"
+        @posts = Post.all.where(status: "craft").check_who_can_see(current_user).order(comments_count: :desc).page(params[:page]).per(20)
+      elsif @order_tag == "impressionist_count"
+        @posts = Post.all.where(status: "craft").check_who_can_see(current_user).order(impressionist_count: :desc).page(params[:page]).per(20)
+      else
+        @posts = Post.all.where(status: "craft").check_who_can_see(current_user).order(id: :desc).page(params[:page]).per(20)
+      end
+      
     else
       @posts = Post.all.where(status: "craft", who_can_see: "all").order(id: :desc).page(params[:page]).per(20)
     end
